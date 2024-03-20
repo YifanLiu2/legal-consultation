a882c046d806af9d835c0ab53d7206d044931c26
Yifan Liu <yifanliu.liu@mail.utoronto.ca>
2024-03-19 23:49:35 -0400

    finish model finetuner

--- code/fine_tune.py
+++ code/fine_tune.py
@@ -1,124 +1,139 @@
-from transformers import AutoTokenizer, AutoModel
-import torch
+import argparse, os
 import pandas as pd
-from torch.utils.data import DataLoader, Dataset, TensorDataset
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, AdamW, get_linear_schedule_with_warmup
-from transformers import BertForSequenceClassification, Trainer, TrainingArguments, DataCollatorWithPadding
-from torch.nn import CrossEntropyLoss
+import torch
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
 
 
-class ConversationDataset(Dataset):
-     def __init__(self, file_path, tokenizer, max_length=256):
-         self.tokenizer = tokenizer
-         self.max_length = max_length
-         self.data = pd.read_csv(file_path)
-         self.encodings = self.tokenizer(self.data['CleanedText'].tolist(), truncation=True, padding=True, 
-                                         max_length=max_length, return_tensors="pt")
-         self.labels = torch.tensor(self.data['WhosePost'].values.astype(int))
-     def __getitem__(self, idx):
-         item = {key: val[idx] for key, val in self.encodings.items()}
-         item['labels'] = self.labels[idx]
-         return item
+class PostsDataset(Dataset):
+    """
+    A PyTorch Dataset class that loads posts data from a CSV file.
+    """
+    def __init__(self, file_path, tokenizer, label_col):
+        self.tokenizer = tokenizer
+        dataset = pd.read_csv(file_path)
 
-     def __len__(self):
-         return len(self.labels)
-     
-class QuestionDataset(Dataset):
-     def __init__(self, file_path, tokenizer, max_length=256):
-         self.tokenizer = tokenizer
-         self.max_length = max_length
-         
-         df = pd.read_csv(file_path)
-         grouped_df = df.groupby('QuestionUno')['CleanedText'].apply(lambda texts: ' '.join(texts)).reset_index()
-         grouped_df = grouped_df.merge(df[['QuestionUno', 'Category']].drop_duplicates(), on='QuestionUno', how='left')
-         
-         self.label_mapping = {label: idx for idx, label in enumerate(grouped_df['Category'].unique())}
-         grouped_df['Category'] = grouped_df['Category'].map(self.label_mapping)
-         self.encodings = self.tokenizer(grouped_df['CleanedText'].tolist(), padding=True, truncation=True, max_length=self.max_length, return_tensors="pt")
-         self.labels = torch.tensor(grouped_df['Category'].values, dtype=torch.long)
-     
-     def __getitem__(self, idx):
-        item = {key: self.encodings[key][idx] for key in self.encodings}
+        posts = dataset["CleanedText"]
+        if label_col not in dataset.columns:
+            raise ValueError(f"'{label_col}' not found in dataset columns. Available columns are: {list(dataset.columns)}")
+        labels = dataset[label_col]
+        self.num_labels = len(labels.unique())
+
+        self.encodings = self.tokenizer(posts.tolist(), truncation=True, padding=True, max_length=512, return_tensors="pt")
+        self.labels = torch.tensor(labels.values.astype(int).tolist(), dtype=torch.long)
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
         item['labels'] = self.labels[idx]
         return item
 
-     def __len__(self):
-         return len(self.labels)
+    def __len__(self):
+        return len(self.labels)
+     
+
+class QuestionDataset(Dataset):
+    """
+    A PyTorch Dataset class that loads text data grouped by questions from a CSV file.
+    """
+    def __init__(self, file_path, tokenizer, label_col):
+        self.tokenizer = tokenizer
+        dataset = pd.read_csv(file_path)
+
+        if label_col not in dataset.columns:
+            raise ValueError(f"'{label_col}' not found in dataset columns. Available columns are: {list(dataset.columns)}")
+        labels = dataset[label_col]
+        self.num_labels = len(labels.unique())
+        
+        # concatenate all posts for the given question with [SEP] tokens
+        questions_data = dataset.groupby("QuestionUno")['CleanedText'].apply(lambda texts: tokenizer.sep_token.join(texts.astype(str))).reset_index()
+        questions_data = questions_data.merge(dataset[["QuestionUno", label_col]].drop_duplicates(), on="QuestionUno", how='left')
+
+        # convert categorical labels to integer
+        unique_labels = questions_data[label_col].unique()
+        label_mapping = {label: idx for idx, label in enumerate(unique_labels)}
+        questions_data[label_col] = questions_data[label_col].map(label_mapping)
+
+        questions = questions_data["CleanedText"]
+        labels = questions_data[label_col]
+        
+        self.encodings = self.tokenizer(questions.to_list(), padding=True, truncation=True, max_length=512, return_tensors="pt")
+        self.labels = torch.tensor(labels.values.astype(int).tolist(), dtype=torch.long)        
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = self.labels[idx]
+        return item
+
+    def __len__(self):
+        return len(self.labels)
 
 
 class ModelFineTuner:
     """
     A class for fine-tuning pre-trained models for various sequence classification tasks.
-
-    Attributes:
-        model_name (str): The pre-trained model to use.
-        num_labels (int): Number of labels for the classification task.
-        max_length (int): Maximum length of the input sequences. Defaults to 512.
-        tokenizer: Tokenizer associated with the pre-trained model.
-        model: The pre-trained model loaded for sequence classification, adapted for `num_labels`.
-        device: The device on which the model will be run.
     """
-    def __init__(self, model_name, num_labels, max_length=512):
-        self.model_name = model_name
-        self.num_labels = num_labels
-        self.max_length = max_length
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = BertForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model.to(self.device)
+    def __init__(self, model, output_dir):
+        self.model = model
+        self.output_dir = output_dir
     
-
     def train(self, train_data):
+        """
+        Trains the model using the specified training data.
 
-        model = AutoModelForSequenceClassification.from_pretrained('nlpaueb/legal-bert-base-uncased', num_labels=2)
-
+        Args:
+            train_data (Dataset): The dataset for training the model.
+        """
         training_args = TrainingArguments(
-            output_dir='/Users/haobao/legal-consultation/data',          
+            output_dir=self.output_dir,          
             num_train_epochs=3,              
             per_device_train_batch_size=8,  
-            logging_dir='/Users/haobao/legal-consultation/data',            
+            gradient_accumulation_steps=8,
+            warmup_steps=500, 
+            weight_decay=0.01,   
+            save_strategy="epoch"         
         )
 
         trainer = Trainer(
-            model=model,                         
+            model=self.model,                         
             args=training_args,                  
-            train_dataset=train_data,       
+            train_dataset=train_data    
         )
 
         trainer.train()
 
-    def train2(self, train_data):
-        df = pd.read_csv(dataset_path)
-        grouped_df = df.groupby('QuestionUno')['CleanedText'].apply(lambda texts: ' '.join(texts)).reset_index()
-        grouped_df = grouped_df.merge(df[['QuestionUno', 'Category']].drop_duplicates(), on='QuestionUno', how='left')
-        model = BertForSequenceClassification.from_pretrained('nlpaueb/legal-bert-base-uncased', num_labels=grouped_df['Category'].nunique())
-        
-        training_args = TrainingArguments(
-            output_dir="/Users/haobao/legal-consultation/data",
-            num_train_epochs=3,
-            per_device_train_batch_size=8,
-            warmup_steps=500,
-            weight_decay=0.01,
-            logging_dir="/Users/haobao/legal-consultation/data",
-            logging_steps=10,
-        )
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            train_dataset=train_data,
-        )
 
-        trainer.train()
+def main(args):
+    model_path = args.model
+    data_path = args.input
+    output_dir = args.output
 
-    def save_model(self, path):
-        self.model.save_pretrained('/Users/haobao/legal-consultation/data')
-        self.tokenizer.save_pretrained('/Users/haobao/legal-consultation/data')
+    if not os.path.exists(model_path):
+        raise ValueError("Bad input path.")
+    
+    # make output dir
+    os.makedirs(output_dir, exist_ok=True)
+
+    # pretrain task1
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    post_dataset = PostsDataset(data_path, tokenizer, "WhosePost")
+    model = BertForSequenceClassification.from_pretrained(model_path, num_labels=post_dataset.num_labels)
+    fine_tunner = ModelFineTuner(model, output_dir)
+    fine_tunner.train(post_dataset)
+
+    # pretrain task2
+    # load model from output_dir this time
+    question_dataset = QuestionDataset(data_path, tokenizer, "Category")
+    model = BertForSequenceClassification.from_pretrained(output_dir, num_labels=question_dataset.num_labels)
+    fine_tunner = ModelFineTuner(model, output_dir)
+    fine_tunner.train(question_dataset)
 
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("nlpaueb/legal-bert-base-uncased")
-    dataset_path = "/Users/haobao/Desktop/ECO482/Research Project/Code/Data/sample_dataset.csv"
-    train_dataset = ConversationDataset(dataset_path, tokenizer, max_length=256)
-    tunner = ModelFineTuner("nlpaueb/legal-bert-base-uncased", num_labels=2, max_length=512)
-    tunner.train(train_dataset)
+    parser = argparse.ArgumentParser(description="Fine-tune a model on two sequential tasks.")
+    parser.add_argument("-i", "--input", type=str, default="data/cleaned_posts.csv", help="Path to the input data CSV file.")
+    parser.add_argument("-o", "--output", type=str, default="models/", help="Directory where outputs will be saved.")
+    parser.add_argument("-m", "--model", type=str, default="nlpaueb/legal-bert-base-uncased", help="Model path or identifier.")
 
+    args = parser.parse_args()
+
+    main(args)
